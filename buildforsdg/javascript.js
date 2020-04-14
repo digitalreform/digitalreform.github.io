5ae607b2778ad0ba9d4916a6451d220b6691a465
Corrie <corriewsnyman@gmail.com>
2020-04-14 09:35:24 +0200

    Added Test Data to javascript.js

--- buildforsdg/javascript.js
+++ buildforsdg/javascript.js
@@ -1,3 +1,44 @@
+// This is Test data that is replaced into the result to enable the page to work without an actual endpoint
+
+let test_result =
+{
+    "data": {
+        "region": {
+            "name": "Africa",
+            "avgAge": 19.7,
+            "avgDailyIncomeInUSD": 5,
+            "avgDailyIncomePopulation": 0.71
+        },
+        "periodType": "days",
+        "timeToElapse": 58,
+        "reportedCases": 674,
+        "population": 66622705,
+        "totalHospitalBeds": 1380614
+    },
+    "impact": {
+        "currentlyInfected": 6740,
+        "infectionsByRequestedTime": 3533701120,
+        "severeCasesByRequestedTime": 530055168,
+        "hospitalBedsByRequestedTime": 529902955,
+        "casesForICUByRequestedTime": 176685056,
+        "casesForVentilatorsByRequestedTime": 10601103,
+        "dollarsInFlight": 14409616760549518
+    },
+    "severeImpact": {
+        "currentlyInfected": 33700,
+        "infectionsByRequestedTime": 17668505600,
+        "severeCasesByRequestedTime": 2650275840,
+        "hospitalBedsByRequestedTime": 2650123627,
+        "casesForICUByRequestedTime": 883425280,
+        "casesForVentilatorsByRequestedTime": 53005516,
+        "dollarsInFlight": 72048083802747584
+    }
+}
+
+let test_url = 'https://jsonplaceholder.typicode.com/posts'
+// End of Test Data
+
+
 function PopulateData(data){
      
     document.getElementById("currentInfected").innerText = data.impact.currentlyInfected.toLocaleString();
@@ -28,12 +69,18 @@ $('#data-go-estimate').click(function(){
     let timeType = document.getElementById("data-period-type").value;
 
     //Setup and execute http request
-    const url= 'https://jsonplaceholder.typicode.com/posts';
+    
+    const url = 'http://www.endpointurl.com?population=' + population + '&reportedCases=' + reported + 
+    '&totalHospitalBeds=' + beds + '&timeToElapse=' + time + '&periodType=' + timeType;
+    
+    //url is console.logged to check if url construction is valid
+    console.log(url);
 
+    //test_url and test_result should be replaced by url and result respectively below after testing if and
+    //end point is available
+    $.getJSON(test_url, function(result){
 
-    $.getJSON(url, function(result){
-
-        //PopulateData(result);
+    PopulateData(test_result);
     })
 
 })
